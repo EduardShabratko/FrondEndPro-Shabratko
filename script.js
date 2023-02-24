c6d18fa006fb12747dac1576b84739a2cf7da0d0
Eduard <shabratkoed@gmail.com>
2023-02-24 22:21:44 +0200

    first commit

---
+++ script.js
@@ -0,0 +1,11 @@
+const firstNumber = prompt('What is your first number?');  
+  
+const secondNumber = prompt('What is your second number?');  
+ 
+const Sum =  Number(firstNumber) + Number(secondNumber); 
+const Diff = firstNumber - secondNumber; 
+const Mult = firstNumber * secondNumber; 
+const Div = firstNumber / secondNumber; 
+  
+  
+alert('Calculations являются finished!' + 'Sum' + '=' + Sum + 'Diff' + '=' + Diff + 'Mult' + '=' + Mult + 'Div' + '=' + Div);
